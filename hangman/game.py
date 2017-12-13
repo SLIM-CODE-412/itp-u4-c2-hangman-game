424600979ea29684d0ecb3f51179fce8b24054db
RAYYYY <ray.sommer@gmail.com>
2017-12-13 17:34:11 +0000

    Hangman working

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,83 @@
 from .exceptions import *
+from  random import randint
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ['What', 'Python', 'Php', 'Computer', 'Pringle', 'Turner', 'Penguins']
 
 
 def _get_random_word(list_of_words):
-    pass
+    if len(list_of_words) == 0:
+        raise InvalidListOfWordsException
+    random_word = randint(0, len(list_of_words)-1)
+    answer_word = list_of_words[random_word]
+
+    return answer_word
 
 
 def _mask_word(word):
-    pass
+    masked_word = ''
+    if word == '':
+        raise InvalidWordException
+    for i in range(len(word)):
+        masked_word += '*'
+    return masked_word
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if len(answer_word) != len(masked_word):
+        raise InvalidWordException
+    if len(character) > 1:
+        raise InvalidGuessedLetterException
+    if answer_word == '' or masked_word == '':
+        raise InvalidWordException
+    uncover_word = ''
+    for i in answer_word:
+        if i.lower() == character.lower():
+            uncover_word += character.lower()
+        elif i.lower() in masked_word:
+            uncover_word += i.lower()
+        else:
+            uncover_word += '*'
+    return uncover_word
+        
 
 
-def guess_letter(game, letter):
-    pass
+def guess_letter(game, letter):##holding the value, im reassigning it each time. need to update the masked word each time?
+    letter1 = letter.lower()
+    
+    if game['remaining_misses'] == 0:
+        raise GameFinishedException
+
+    if game['answer_word'].lower() == game['masked_word']:
+        raise GameFinishedException
+        
+    if letter not in game['previous_guesses']:
+        game['previous_guesses'].append(letter1)
+    uncover_word = ''   
+    for i in game['answer_word'].lower():
+        if i not in game['masked_word']:
+            if i.lower() == letter1:
+                uncover_word += letter1
+            else:
+                uncover_word += "*"
+        else:
+            uncover_word += i
+    game['masked_word'] = uncover_word
+    miss_list = []
+    for i in game['answer_word']:
+        miss_list.append(i.lower())
+    if letter1 not in miss_list:
+        game['remaining_misses'] -= 1
+    
+    if game['answer_word'] == game['masked_word']:
+        raise GameWonException
+        
+    if game['remaining_misses'] == 0:
+        raise GameLostException
+    
+    
+            
+ 
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
